a9d2f089c34485a46fcc0e06351570602f2a349a
Sebastian <elvaxumb@gmail.com>
2018-05-06 22:12:23 +0200

    Initial import

---
+++ hmap.c
@@ -0,0 +1,139 @@
+/*
+ * Autor: Sebastian Pabich
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include "hmap.h"
+#include "alg_linked_list.h"
+#include "alg_utility.h"
+#include "external.h"
+
+int test = 10;
+
+struct hmap{
+    int size,
+        no_elements;
+    struct node_hmap *arr;
+};
+
+struct node_hmap{
+    int size;
+    void* data_structure;
+    functions *functions;
+};
+
+
+void print_alldata_node_hmap(hmap* hmap1, int index){
+    hmap1->arr[index].functions->print_all_data(hmap1->arr[index].data_structure);
+}
+
+int get_size_node_hmap(hmap* hmap1, int index){
+    return hmap1->arr[index].size;
+}
+
+hmap *new_hash_map(int size){
+    hmap *new = (hmap *) malloc(sizeof(hmap));
+    new->size = size;
+    new->no_elements = 0;
+    new->arr = create_hash_table(size);
+}
+
+node_hmap* create_hash_table(int size){
+    node_hmap* table = (node_hmap *) malloc(sizeof(node_hmap) * size);
+
+    for (int i = 0; i < size; i++) {
+        table[i].size = 0;
+        table[i].data_structure = NULL;
+        table[i].functions = NULL;
+    }
+    return table;
+}
+
+unsigned long hash(unsigned char *str)
+{
+    unsigned long hash = 5381;
+    int c;
+
+    while (c = *str++)
+        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+
+    return hash % MOD;
+}
+
+//poprawic inkrementacje size
+int insert_hmap(void* data_structure, char* data){
+    hmap *hmap = data_structure;
+    long hash_index = hash(data);
+
+    //if sprawdzający size i zmieniający strukture
+
+    if(hmap->arr[hash_index].data_structure == NULL){
+        hmap->arr[hash_index].data_structure = new_node_list(data, NULL);
+        hmap->arr[hash_index].functions = &functions_list;
+        hmap->arr[hash_index].size++;
+        increment_elements_hmap(hmap);
+    } else if (hmap->arr[hash_index].size > 2) {
+        if ( hmap->arr[hash_index].functions->insert(hmap->arr[hash_index].data_structure, data)){
+            hmap->arr[hash_index].data_structure = convert_to_rbt(hmap, hash_index);
+            hmap->arr[hash_index].functions = &functions_rbt;
+            hmap->arr[hash_index].size++;
+            increment_elements_hmap(hmap);
+        }
+    } else {
+        if (hmap->arr[hash_index].functions->insert(hmap->arr[hash_index].data_structure, data)){
+            hmap->arr[hash_index].size++;
+            increment_elements_hmap(hmap);
+        }
+    }
+    return 1;
+
+}
+
+node_rbt* convert_to_rbt(hmap *hmap1, int hash_index){
+    node_list *head_list = get_data_structure_hmap(hmap1, hash_index);
+    node_rbt *new_data_structure = create_bst(get_data_list(head_list));
+
+    node_list *current = head_list;
+    while ((current = get_next(current)) != NULL){
+        insert_rbt(new_data_structure, get_data_list(current));
+    }
+    // TODO delete list
+    return new_data_structure;
+}
+
+void* get_data_structure_hmap(hmap *hmap1, int hash_index){
+    return hmap1->arr[hash_index].data_structure;
+}
+
+void delete_hmap(struct hmap* hmap, char* data){
+    long hash_index = hash(data);
+
+    hmap->arr[hash_index].data_structure =
+            hmap->arr[hash_index].functions->delete(hmap->arr[hash_index].data_structure, data);
+
+    hmap->arr[hash_index].size = hmap->arr[hash_index].functions->count_elements(hmap->arr[hash_index].data_structure);
+}
+
+
+void printTest(){
+    printf("test = %d\n", test);
+}
+
+void increment_elements_hmap(hmap *hmap1){
+    hmap1->no_elements++;
+}
+
+int get_no_elements_hmap(hmap *hmap1){
+    return hmap1->no_elements;
+}
+
+int find_hmap(void* hmap1, char* data){
+    hmap* hash_map = hmap1;
+    long hash_index = hash(data);
+
+    //size 0 of node in hash table indicates no elements in that node
+    if(hash_map->arr[hash_index].size == 0)
+        return 0;
+    //otherwise find element
+    return hash_map->arr[hash_index].functions->find(hash_map->arr[hash_index].data_structure, data);
+}
---
+++ hmap.h
@@ -0,0 +1,78 @@
+/*
+ * Autor: Sebastian Pabich
+ */
+//
+// Created by sebastian on 28.04.18.
+//
+
+#ifndef CLIONLISTA4_HMAP_H
+#define CLIONLISTA4_HMAP_H
+
+#include "alg_rbt.h"
+
+#define MOD 10
+
+extern int test;
+
+//typedef struct functions Functions;
+
+//typedef struct collection_functions collection_functions;
+
+//extern struct collection_functions *list_functions;
+//extern struct collection_functions *rbt_functions;
+
+
+
+//typedef struct hmap{
+//    int size;
+//    struct node_hmap *arr;
+//}hmap;
+//
+//typedef struct node_hmap{
+//    int size;
+//    void* data_structure;
+//    struct functions* functions;
+//}node_hmap;
+
+typedef struct hmap hmap;
+
+typedef struct node_hmap node_hmap;
+
+unsigned long hash(unsigned char *str);
+
+hmap* new_hash_map(int size);
+
+node_hmap* create_hash_table(int size);
+
+int get_size_node_hmap(hmap* hmap1, int index);
+
+void print_alldata_node_hmap(hmap* hmap1, int index);
+
+void printTest();
+
+node_rbt* convert_to_rbt(hmap *hmap1, int hash_index);
+
+void* get_data_structure_hmap(hmap *hmap1, int hash_index);
+
+void increment_elements_hmap(hmap *hmap1);
+
+
+int insert_hmap(void* hmap, char* data);
+void delete_hmap(struct hmap* hmap, char* data);
+//void load_hmap(struct hmap* hmap)
+
+
+int get_no_elements_hmap(hmap *hmap1);
+
+/**
+ * Function searches data structure for given data
+ * @param hmap  pointer to hash map
+ * @param data  value to look for
+ * @return      1 if value exists in hash map
+ *              0 otherwise
+ */
+int find_hmap(void* hmap, char* data);
+
+//void insert(void* data_structure, char* data);
+
+#endif //CLIONLISTA4_HMAP_H
