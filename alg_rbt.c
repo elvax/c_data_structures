f5da1d442fd9f447fa843364cb8df301c6e76a98
Sebastian <elvaxumb@gmail.com>
2018-05-08 18:47:07 +0200

    Fix insertion and get rid off unnecessary functions

--- alg_rbt.c
+++ alg_rbt.c
@@ -33,12 +33,6 @@ rb_tree *new_rb_tree(){
     return new;
 }
 
-node_rbt *create_bst(char *data){
-    node_rbt *new_root = new_leaf_rbt(&NIL, data);
-    new_root->color = BLACK;
-    return new_root;
-}
-
 node_rbt* new_node_rbt(node_rbt *parent, char* data){
     node_rbt *new = (node_rbt *) malloc( sizeof(node_rbt) );
     new->parent = parent;
@@ -59,33 +53,7 @@ node_rbt* new_leaf_rbt(node_rbt* parent, char* data){
     return leaf;
 }
 
-int _insert_rbt(node_rbt *root, node_rbt *new_node){
-    node_rbt *parent = NULL;
-    node_rbt *current = root;
-    int isLeft = 0;
-    while (current != &NIL) {
-        parent = current;
-        if (strcmp(new_node->data, current->data) < 0) {
-            current = current->left_child;
-            isLeft = 1;
-        } else if (strcmp(new_node->data, current->data) > 0){
-            current = current->right_child;
-            isLeft = 0;
-        } else {
-            return 0;
-        }
-    }
-    if(isLeft){
-        new_node->parent = parent;
-        parent->left_child = new_node;
-    } else {
-        new_node->parent = parent;
-        parent->right_child = new_node;
-    }
-    return 1;
-}
-
-void fix_rbt(node_rbt *root){
+void fix_rbt(rb_tree* tree, node_rbt *root){
     node_rbt *current = root;
     while (current->parent->color == RED) {
         if (current->parent == current->parent->parent->left_child) {
@@ -97,11 +65,12 @@ void fix_rbt(node_rbt *root){
                 current = current->parent->parent;
             } else if (current == current->parent->right_child) {
                 current = current->parent;
-                rotate_left(current);
+                rotate_left(tree, current);
+            } else {
+                current->parent->color = BLACK;
+                current->parent->parent->color = RED;
+                rotate_right(tree, current->parent->parent);
             }
-            current->parent->color = BLACK;
-            current->parent->parent->color = RED;
-            rotate_right(current->parent->parent);
         } else {
             node_rbt *y = current->parent->parent->left_child;
             if (y->color == RED) {
@@ -111,14 +80,15 @@ void fix_rbt(node_rbt *root){
                 current = current->parent->parent;
             } else if (current == current->parent->left_child) {
                 current = current->parent;
-                rotate_left(current);
+                rotate_right(tree, current);
+            } else {
+                current->parent->color = BLACK;
+                current->parent->parent->color = RED;
+                rotate_left(tree, current->parent->parent);
             }
-            current->parent->color = BLACK;
-            current->parent->parent->color = RED;
-            rotate_right(current->parent->parent);
         }
-        root->color = BLACK;
     }
+    tree->root->color = BLACK;
 }
 
 int insert_rbt(void *ds, char *data){
@@ -128,111 +98,35 @@ int insert_rbt(void *ds, char *data){
         printf("error null pointer\n");
         exit(-1);
     }
+        if (rbTree->root != NULL){
+            node_rbt *new_node = new_leaf_rbt(&NIL, data);
+            node_rbt *parent = &NIL;
+            node_rbt *current = rbTree->root;
+            while (current != &NIL) {
+                parent = current;
+                if (strcmp(new_node->data, current->data) < 0){
+                    current = current->left_child;
+                } else {
+                    current = current->right_child;
+                }
+            }
+            new_node->parent = parent;
+            if (strcmp(new_node->data, parent->data) < 0){
+                parent->left_child = new_node;
+            } else {
+                parent->right_child = new_node;
+            }
 
-    if (rbTree->root == NULL || rbTree->no_elements == 0) {
-        rbTree->root = create_bst(data);
-        rbTree->no_elements++;
+            rbTree->no_elements++;
+            fix_rbt(rbTree, new_node);
     } else {
-        node_rbt *new_node = new_leaf_rbt(NULL, data);
-        int status = _insert_rbt(rbTree->root, new_node);
+        rbTree->root = new_leaf_rbt(&NIL, data);
+        rbTree->root->color = BLACK;
         rbTree->no_elements++;
-
-        insert_repair_tree(new_node);
-
-        return status;
     }
 }
 
-void insert_repair_tree(node_rbt* n) {
-    if (parent(n) == NULL) {
-        insert_case1(n);
-    } else if (parent(n)->color == BLACK) {
-        insert_case2(n);
-    } else if (uncle(n)->color == RED) {
-        insert_case3(n);
-    } else {
-        insert_case4(n);
-    }
-}
-
-void insert_case1(node_rbt* n)
-{
-    if (parent(n) == NULL)
-        n->color = BLACK;
-}
-
-void insert_case2(node_rbt* n)
-{
-    return; /* Do nothing since tree is still valid */
-}
-
-void insert_case3(node_rbt* n)
-{
-    parent(n)->color = BLACK;
-    uncle(n)->color = BLACK;
-    grandparent(n)->color = RED;
-    insert_repair_tree(grandparent(n));
-}
-
-void insert_case4(node_rbt* n)
-{
-    node_rbt* p = parent(n);
-    node_rbt* g = grandparent(n);
-
-    if (n == g->left_child->right_child) {
-        rotate_left(p);
-        n = n->left_child;
-    } else if (n == g->right_child->left_child) {
-        rotate_right(p);
-        n = n->right_child;
-    }
-
-    insert_case4step2(n);
-}
-
-void insert_case4step2(node_rbt* n)
-{
-    node_rbt* p = parent(n);
-    node_rbt* g = grandparent(n);
-
-    if (n == p->left_child)
-        rotate_right(g);
-    else
-        rotate_left(g);
-    p->color = BLACK;
-    g->color = RED;
-}
-
-node_rbt* parent(node_rbt* n) {
-    return n->parent;
-}
-
-node_rbt* grandparent(node_rbt* n) {
-    node_rbt* p = parent(n);
-    if (p == NULL)
-        return NULL; // No parent means no grandparent
-    return parent(p);
-}
-
-node_rbt* sibling(node_rbt* n) {
-    node_rbt* p = parent(n);
-    if (p == NULL)
-        return NULL; // No parent means no sibling
-    if (n == p->left_child)
-        return p->right_child;
-    else
-        return p->left_child;
-}
-
-node_rbt* uncle(node_rbt* n) {
-    node_rbt* p = parent(n);
-    node_rbt* g = grandparent(n);
-    if (g == NULL)
-        return NULL; // No grandparent means no uncle
-    return sibling(p);
-}
-
-void rotate_left(node_rbt* x) {
+void rotate_left(rb_tree* tree, node_rbt* x) {
     node_rbt *y = x->right_child;
     x->right_child = y->left_child;
     if (y->left_child != &NIL) {
@@ -241,16 +135,17 @@ void rotate_left(node_rbt* x) {
     y->parent = x->parent;
     if (x->parent == &NIL) {
         x->parent = y;
+        tree->root = y;
     } else if (x == x->parent->left_child) {
         x->parent->left_child = y;
     } else {
         x->parent->right_child = y;
     }
-    x->left_child = x;
+    y->left_child = x;
     x->parent = y;
 }
 
-void rotate_right(node_rbt* y) {
+void rotate_right(rb_tree* tree, node_rbt* y) {
     node_rbt *x = y->left_child;
     y->left_child = x->right_child;
     if (x->right_child != &NIL) {
@@ -259,12 +154,13 @@ void rotate_right(node_rbt* y) {
     x->parent = y->parent;
     if (y->parent == &NIL) {
         y->parent = x;
+        tree->root = x;
     } else if (y == y->parent->right_child) {
         y->parent->right_child = x;
     } else{
         y->parent->left_child = x;
     }
-    y->right_child = y;
+    x->right_child = y;
     y->parent = x;
 
 }
@@ -284,9 +180,9 @@ void print_inorder_rbt(void *ds){
 
 void _print_inorder_rbt(node_rbt *root){
     node_rbt *current = root;
-    if (root != NULL) {
+    if (root != NULL && root != &NIL) {
         _print_inorder_rbt(current->left_child);
-        printf("%s %d \n", current->data, current->color);
+        printf("rbt: %s %d \n", current->data, current->color);
         _print_inorder_rbt(current->right_child);
     }
 }
@@ -400,28 +296,3 @@ node_rbt* get_node_of_val_rbt(void* ds, char* data){
     }
     return current;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- alg_rbt.h
+++ alg_rbt.h
@@ -22,35 +22,14 @@ node_rbt* new_node_rbt(node_rbt*, char*);
 
 node_rbt* new_leaf_rbt(node_rbt*, char*);
 
-int _insert_rbt(node_rbt *root, node_rbt *new_node);
 
 int insert_rbt(void *root, char *data);
 
-void fix_rbt(node_rbt *root);
+void fix_rbt(rb_tree* tree, node_rbt *root);
 
-void insert_repair_tree(node_rbt* n);
+void rotate_left(rb_tree* tree, node_rbt* n);
 
-void insert_case1(node_rbt* n);
-
-void insert_case2(node_rbt* n);
-
-void insert_case3(node_rbt* n);
-
-void insert_case4(node_rbt* n);
-
-void insert_case4step2(node_rbt* n);
-
-struct node_rbt* parent(node_rbt* n);
-
-struct node_rbt* grandparent(node_rbt* n);
-
-struct node_rbt* sibling(node_rbt* n);
-
-struct node_rbt* uncle(node_rbt* n);
-
-void rotate_left(node_rbt* n);
-
-void rotate_right(node_rbt* n);
+void rotate_right(rb_tree* tree, node_rbt* n);
 
 void print_inorder_rbt(void *ds);
 
